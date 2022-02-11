ca7ab65e2b6828d3837567be0e00ef7765f813fc
Matatjahuu <52283149+Matatjahuu@users.noreply.github.com>
2022-02-11 10:08:53 +0100

    what is it for?

--- src/main/java/pl/vertty/arivi/managers/TablistManager.java
+++ src/main/java/pl/vertty/arivi/managers/TablistManager.java
@@ -12,19 +12,7 @@ public class TablistManager
         if (RankingManager.getRankings().size() < i) {
             return "&7" + i + ". &fBrak informacji.";
         }
-        String s;
-        if (i == 1) {
-            s = "&7" + i + ". ";
-        }
-        else if (i == 2) {
-            s = "&7" + i + ". ";
-        }
-        else if (i == 3) {
-            s = "&7" + i + ". ";
-        }
-        else {
-            s = "&7" + i + ". ";
-        }
+        String s = "&7" + i + ". ";
         return ChatUtil.fixColor(s + "&f" + RankingManager.getRankings().get(i - 1).getName() + " &8- &7" + RankingManager.getRankings().get(i - 1).getPoints() + "pkt");
     }
     
@@ -32,22 +20,8 @@ public class TablistManager
         if (RankingManager.getGuildRankings().size() < i) {
             return "&7" + i + ". &fBrak informacji.";
         }
-        String s;
-        if (i == 1) {
-            s = "&7" + i + ". ";
-        }
-        else if (i == 2) {
-            s = "&7" + i + ". ";
-        }
-        else if (i == 3) {
-            s = "&7" + i + ". ";
-        }
-        else {
-            s = "&7" + i + ". ";
-        }
-        if (RankingManager.getGuildRankings().get(i - 1).getOnlineMembers().size() != 0) {
-            return ChatUtil.fixColor(s + "&f" + RankingManager.getGuildRankings().get(i - 1).getTag() + " &8- &7" + RankingManager.getGuildRankings().get(i - 1).getPoints() + "pkt");
-        }
+        String s = "&7" + i + ". ";
+        RankingManager.getGuildRankings().get(i - 1).getOnlineMembers();
         return ChatUtil.fixColor(s + "&f" + RankingManager.getGuildRankings().get(i - 1).getTag() + " &8- &7" + RankingManager.getGuildRankings().get(i - 1).getPoints() + "pkt");
     }
 }
